2232127de5045a79cd955e5a5ed1a5824b49861a
Ashwin Narayan <ashwinnarayan1994@gmail.com>
2022-07-28 14:20:32 +0800

    Wallet connect / disconnect works now.

--- src/Components/AppBar.tsx
+++ src/Components/AppBar.tsx
@@ -13,14 +13,24 @@ import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 
-import { WalletDialogProvider, WalletMultiButton } from '@solana/wallet-adapter-material-ui';
+import { useWalletDialog, WalletDialogProvider, WalletIcon, WalletMultiButton } from '@solana/wallet-adapter-material-ui';
+import { useWallet } from '@solana/wallet-adapter-react';
+import { useMemo, useState } from 'react';
+
+import { AiOutlineWallet } from 'react-icons/ai'
+import { yellow } from '@mui/material/colors';
 
 const pages = ['About', 'Contact Me'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const settings = ['Connect Wallet'];
 
 const ResponsiveAppBar = () => {
   const [anchorElNav, setAnchorElNav]   = React.useState<null | HTMLElement>(null);
   const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+  const { publicKey, wallet, connect, connecting, connected, disconnect, disconnecting } = useWallet();
+  const { setOpen } = useWalletDialog();
+  const [anchor, setAnchor] = useState<HTMLElement>();
+
+  const base58 = useMemo(() => publicKey?.toBase58(), [publicKey]);
 
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElNav(event.currentTarget);
@@ -36,6 +46,18 @@ const ResponsiveAppBar = () => {
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
+
+  function handleWalletSelect(event: any) {
+    setOpen(true);
+  }
+
+  function handleWalletConnect(event: any) {
+    connect().catch(()=> {});
+  }
+
+  function handleWalletDisconnect(event: any) {
+    disconnect().catch(() => {});
+  }
 
   return (
     <AppBar position="static">
@@ -126,10 +148,15 @@ const ResponsiveAppBar = () => {
             ))}
           </Box>
 
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+          <Box sx={{ flexGrow: 0, bgcolor: yellow[200] }}>
+            <Tooltip title="Connect Wallet">
+              <IconButton onClick={wallet?handleOpenUserMenu:handleWalletSelect} sx={{ p: 0 }}>
+                <Avatar sx={{ bgcolor: yellow[200] }}>
+                  { wallet ? 
+                    <WalletIcon wallet={wallet}/> :
+                    <AiOutlineWallet/>
+                  }
+                </Avatar>
               </IconButton>
             </Tooltip>
             <Menu
@@ -148,11 +175,19 @@ const ResponsiveAppBar = () => {
               open={Boolean(anchorElUser)}
               onClose={handleCloseUserMenu}
             >
-              {settings.map((setting) => (
+              <MenuItem onClick={publicKey?handleWalletDisconnect:handleWalletConnect}>
+                <Typography textAlign="center">
+                  { publicKey ? 
+                    "Disconnect Wallet" :
+                    "Connect Wallet"
+                  }
+                </Typography>
+              </MenuItem>
+              {/* {settings.map((setting) => (
                 <MenuItem key={setting} onClick={handleCloseUserMenu}>
                   <Typography textAlign="center">{setting}</Typography>
                 </MenuItem>
-              ))}
+              ))} */}
             </Menu>
           </Box>
         </Toolbar>
